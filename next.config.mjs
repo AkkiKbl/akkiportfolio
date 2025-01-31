51982258e25d1c172135e2a5615b9c5b7e85cfb0
Akshay Kubal <akshay2298060@gmail.com>
2025-01-31 20:21:08 +0530

    second commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,16 @@
 import mdx from "@next/mdx";
 
-const withMDX = mdx({
-  extension: /\.mdx?$/,
-  options: {},
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "md", "mdx"],
 };
 
-export default withMDX(nextConfig);
+const withMDX = mdx({
+  extension: /\.mdx?$/,
+  options: {},
+});
+
+// Combine the configurations using spread operator
+module.exports = withMDX({
+  ...nextConfig,
+});
