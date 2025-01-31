0d114486c421d15c874db0e84f6cf92b48f7c3aa
Akshay Kubal <akshay2298060@gmail.com>
2025-01-31 21:29:05 +0530

    testing

--- next.config.js
+++ next.config.js
@@ -1,23 +1,14 @@
 import mdx from "@next/mdx";
+
 const withMDX = mdx({
   extension: /\.mdx?$/,
   options: {},
 });
 
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  output: "export",
   pageExtensions: ["ts", "tsx", "md", "mdx"],
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
-
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
-
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
 };
 
-module.exports = nextConfig;
+export default withMDX(nextConfig);
